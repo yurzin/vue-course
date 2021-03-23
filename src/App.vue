e4e41e6091adde77c2b9c402de43336362e7ea55
Yurzin Sergey <yurzin.sergey@mail.ru>
2021-03-23 17:26:10 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Todo application</h1>
+        <AddTodo
+            @add-todo="addTodo"
+        />
+        <hr>
+        <TodoList
+        v-bind:todos="todos"
+        @remove-todo="removeTodo"
+        />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import TodoList from '@/components/TodoList'
+    import AddTodo from '@/components/AddTodo'
+    export default {
+        name: 'App',
+        data() {
+            return {
+                todos: [
+                    {id: 1, title: 'Купить хлеб', completed: false},
+                    {id: 2, title: 'Купить масло', completed: false},
+                    {id: 3, title: 'Купить пиво', completed: false}
+                ]
+            }
+        },
+        methods: {
+            removeTodo(id) {
+                this.todos = this.todos.filter(t => t.id !== id)
+            },
+            addTodo(todo) {
+                this.todos.push(todo)
+            }
+        },
+        components: {
+            TodoList, AddTodo
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
